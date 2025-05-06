4eed1b345a20c9d5c477cfba856d606a37a42af0
Chessdipeshgiri <chessblogs246@gmail.com>
2025-05-06 09:33:13 +0545

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), tailwindcss()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    tailwindcss()
+  ],
+  base: process.env.VITE_BASE_PATH || '/demowebsite',
 })
